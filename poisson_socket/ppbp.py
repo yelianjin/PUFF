e362f5e2207c7cf839c66993337b1d3a3b0b2400
yelianjin <yelianjin1994@163.com>
2020-11-10 14:17:30 +0800

    update in poisson-packet by jackljye

--- poisson_socket/ppbp.py
+++ poisson_socket/ppbp.py
@@ -9,21 +9,23 @@ class PPBP():
         ##unit:bit
         self.m_cbrRate=100*1024*1024
         self.m_pktSize=1470
-        self.m_burstArrivals=float(500)
-        self.m_burstlength=float(0.005)
+        self.m_burstArrivals=float(5)
+        self.m_burstlength=float(0.05)
         self.m_h=float(0.7)
         self.m_totalBytes=0
         self.m_activebursts=0
         self.m_offPeriod=True
         self.lastStartTime=time.time()
         self.initialTime=self.lastStartTime
-        self.flowSize=1024*15
+        self.flowSize=100*1024
         self.s=sched.scheduler(time.time,time.sleep)
         self.count_all=int(self.flowSize/1448)
-        self.exp_set=np.random.exponential(float(1/self.m_burstArrivals),10000)
+        self.exp_set=np.random.exponential(float(1/self.m_burstArrivals),1024)
         self.count=0
+        self.count_arrive=0
+        self.count_departure=0
         self.m_shape=3-2*self.m_h
-        self.t_pareto_set=np.random.pareto(self.m_shape,10000)*self.m_burstlength
+        self.t_pareto_set=np.random.pareto(self.m_shape,1024)*self.m_burstlength
         ##No mtxTrace()
         
     def process(self):
@@ -34,17 +36,19 @@ class PPBP():
         #t_poisson_arrival=(np.random.exponential(float(1/self.m_burstArrivals),1))[0]
 		#m_PoissonArrival =s.enter(t_poisson_arrival,1,self.PoissonArrival);
 
-        self.s.enter(self.lastStartTime-self.initialTime+t_poisson_arrival,1,self.PoissonArrival)
+        #self.s.enter(self.lastStartTime-self.initialTime+t_poisson_arrival,1,self.PoissonArrival)
 		#m_timeSlot=(self.m_shape - 1) * self.m_burstLength /self.m_shape
-
+        self.s.enter(t_poisson_arrival,1,self.PoissonArrival)
         ###a:scale
         ###shape: shape
         #t_pareto=(np.random.pareto(self.m_shape,1)*self.m_burstlength)[0]
         t_pareto=self.t_pareto_set[self.count]
-        self.s.enter(self.lastStartTime-self.initialTime+t_poisson_arrival+t_pareto,1,self.ParetoDeParture)
-        self.s.enter(self.lastStartTime-self.initialTime+t_poisson_arrival,1,self.process)
+        #self.s.enter(self.lastStartTime-self.initialTime+t_poisson_arrival+t_pareto,1,self.ParetoDeParture)
+        #self.s.enter(self.lastStartTime-self.initialTime+t_poisson_arrival,1,self.process)
+        self.s.enter(t_poisson_arrival+t_pareto,1,self.ParetoDeParture)
+        self.s.enter(t_poisson_arrival,1,self.process)
         self.count+=1
-        if(self.count>=10000):
+        if(self.count>=1024):
             self.count=0
         if (self.m_totalBytes>=self.flowSize):
             print('HEY2')
@@ -53,13 +57,15 @@ class PPBP():
 
     def PoissonArrival(self):
          self.m_activebursts+=1
+         self.count_arrive+=1
          if(self.m_offPeriod):
              self.ScheduleNextTx()
     
     
     def ParetoDeParture(self):
+        self.count_departure+=1
         self.m_activebursts-=1
-        
+    
     def ScheduleNextTx(self):
          
         ##Schedule Next burst 
@@ -70,8 +76,9 @@ class PPBP():
             print('HI')
             self.m_offPeriod = False
             data_rate=nextTime/self.m_activebursts
-            self.m_lastStartTime=time.time()
-            self.s.enter(self.m_lastStartTime-self.initialTime+data_rate,1,self.send_packet)
+            #self.m_lastStartTime=time.time()
+            #self.s.enter(self.m_lastStartTime-self.initialTime+data_rate,1,self.send_packet)
+            self.s.enter(data_rate,1,self.send_packet)
             print('TEST')
             self.m_totalBytes+=1448
             print(self.m_totalBytes)
@@ -100,9 +107,6 @@ if __name__=="__main__":
     s=sched.scheduler(time.time,time.sleep)
     s.enter(1,1,print_time,kwargs={'a':'1'})
     s.enter(1,1,print_time,kwargs={'a':'2'})
-    print(time.time())
-    s.run()
-    print(time.time())
     """
     b.process()
     #print(b.t_pareto_set)
@@ -119,5 +123,7 @@ if __name__=="__main__":
     print(time.time()-start1)
     print(b.flowSize)
     print(b.m_totalBytes)
+    print(b.count_arrive)
+    print(b.count_departure)
     #print('end')
     #print(time.time()-start)
